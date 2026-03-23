1257960095b3f8c2477ddff990757600a0533b2a
Jose8540 <lopeseletrica@hotmail.com>
2026-03-23 02:51:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'parshall-v1';
+const assets = ['./', './index.html'];
+
+// Instala e faz o cache dos arquivos
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+// Responde mesmo se estiver offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
